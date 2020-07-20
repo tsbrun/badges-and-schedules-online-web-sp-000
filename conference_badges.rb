eafd52d674a51cdb6ff7b1852c815aec312d35c5
Anya Brun <tsbrun@icloud.com>
2020-07-20 20:52:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,5 +16,5 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator.each {|badge| puts badge}
+  puts batch_badge_creator(attendees).each {|badge| puts badge}
 end
